3812982b43335638895c203336f81fe9665a13c8
Blue-hue <sapubuntu3@gmail.com>
2024-03-01 11:19:36 +0530

    added ans1

--- OOS ASS2/MainChild.java
+++ OOS ASS2/MainChild.java
@@ -9,12 +9,7 @@ public class MainChild {
     public static void main(String[] args) {
         Thread t = new Thread(new Child());
         t.start();
-        try{
-            System.out.println("Within main thread");
-            Thread.sleep(1);
-        } catch(InterruptedException ie){
-            System.out.println("Main Thread interrupted");
-        }
+        System.out.println("Within main thread");
         System.out.println("Exiting from main thread");
     }
 }
